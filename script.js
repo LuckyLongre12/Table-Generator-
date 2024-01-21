f54badb6c76f3354c04ddbb455dca4b4003436ec
LuckyLongre12 <146944939+LuckyLongre12@users.noreply.github.com>
2024-01-21 15:24:50 +0530

    Files added

---
+++ script.js
@@ -0,0 +1,26 @@
+function Generate() {
+
+   let Num = document.getElementById('num').value;
+   let Txt1 = document.querySelector('.txt1');   
+   let Txt2 = document.querySelector('.txt2'); 
+   let Txt3 = document.querySelector('.txt3');
+   let Txt4 = document.querySelector('.txt4');   
+   let Txt5 = document.querySelector('.txt5'); 
+   let Txt6 = document.querySelector('.txt6');
+   let Txt7 = document.querySelector('.txt7');   
+   let Txt8 = document.querySelector('.txt8'); 
+   let Txt9 = document.querySelector('.txt9');
+  let Txt10 = document.querySelector('.txt10');    
+      
+      Txt1.innerText= Num*1;
+      Txt2.innerText= Num*2;
+      Txt3.innerText= Num*3;
+      Txt4.innerText= Num*4;
+      Txt5.innerText= Num*5;
+      Txt6.innerText= Num*6;
+      Txt7.innerText= Num*7;
+      Txt8.innerText= Num*8;
+      Txt9.innerText= Num*9;
+      Txt10.innerText= Num*10;
+       
+}
